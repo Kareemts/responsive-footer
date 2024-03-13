550c4d4e79e1954ce2ac054982bfc54ce478720a
abdul <abdul.mykare@gmal.com>
2024-03-13 20:31:04 +0530

    fix: clear the changes based on the feedback

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ export default function App() {
     <section>
       <footer className="relative">
         {/* desktop */}
-        <div className="w-full h-[498px]  px-10 hidden lg:block  pt-20 bg-[#89BF50] fixed bottom-0 text-[#FFFFFF] ">
-          <div className="flex 2xl:justify-center">
-            <div className="flex lg:gap-10 xl:gap-20  ">
+        <div className="w-full h-[498px]   px-10 hidden xl:block  pt-14 bg-[#89BF50] fixed bottom-0 text-[#FFFFFF] ">
+          <div className="flex justify-between gap-10 2xl:gap-20">
+            <div className="flex justify-between w-full gap-14  ">
               <div className="text-[20px] font-[400] text-nowrap">
                 <h4 className="text-[32px] font-[600]">Kontakt</h4>
-                <p className="mt-5">Egestam Mattsson El & Data AB</p>
+                <p className="mt-5 ">Egestam Mattsson El & Data AB</p>
                 <p>Ridspögatan 15a</p>
                 <p>213 77 Malmö</p>
                 <p className="mt-5"> Tel: 040-6170166</p>
@@ -28,8 +28,8 @@ export default function App() {
                 <p>Kontakta oss</p>
                 <p>Jobba med oss</p>
               </div>
-              <div className="text-[18px] mt-[70px] font-[400] flex flex-col gap-2 text-nowrap">
-                <p className="text-[16px] font-[600]">Din integritet</p>
+              <div className="text-[16px] mt-[70px] font-[400] flex flex-col gap-2 text-nowrap">
+                <p className="text-[18px] font-[600]">Din integritet</p>
                 <p>Integritetspolicy</p>
                 <p>Cookiepolicy</p>
                 <div className="flex  items-center gap-2">
@@ -44,8 +44,8 @@ export default function App() {
                 </div>
               </div>
             </div>
-            <div className="flex lg:gap-10 xl:gap-36">
-              <div className="text-[18px] font-[400] ml-6 flex flex-col gap-2">
+            <div className="flex justify-between w-full gap-5 ">
+              <div className="text-[18px] font-[400] flex flex-col gap-2">
                 <p className="text-[32px] font-[600]">Gasellvinnare</p>
                 <img src={gasell} width={268} height={128} alt="gasell" />{" "}
                 <p className="text-[12px] font-[300] xl:w-[284px]">
@@ -55,24 +55,24 @@ export default function App() {
                   på kvalitet, miljömedvetenhet och säkerhet.
                 </p>
               </div>
-              <div className="text-[18px] mt-[100px] ">
+              <div className="text-[18px] mt-[100px] mr-12 2xl:mr-28 ">
                 <img
                   src={em}
                   width={164}
                   height={104}
                   alt="em"
-                  className=" min-w-[80px] w-[164px] h-[104px]"
+                  className=" min-w-[164px] h-[104px]"
                 />{" "}
               </div>
             </div>
           </div>
-          <div className="bg-white h-[1px] absolute bottom-20 right-0 opacity-20 w-[80%]" />
-          <p className="mt-20 text-[20px] font-[400] text-end mr-[250px]">
+          <div className="bg-white h-[1px] absolute bottom-20 right-0 opacity-20 w-full" />
+          <p className="mt-[88px] text-[20px] font-[400] text-center">
             Copyright © 2024 All rights reserved.
           </p>
         </div>
         {/* below tab screen */}
-        <div className="flex  justify-center px-5 md:items-center  md:justify-start lg:hidden h-[597px] bg-[#89BF50] w-full fixed bottom-0 text-[#FFFFFF] ">
+        <div className="flex   px-5 md:items-end md:pb-[40px]  md:justify-start xl:hidden  h-[618px] md:h-[597px] bg-[#89BF50] w-full fixed bottom-0 text-[#FFFFFF] ">
           <div className="flex flex-col  px-5 md:px-8 ">
             <div className="md:text-[16px]  font-[400] text-nowrap">
               <img
@@ -82,14 +82,16 @@ export default function App() {
                 alt="em"
                 className="min-w-[164px] md:hidden mt-3 h-[104px]"
               />{" "}
-              <p className="text-[32px] mt-3 md:mt-0 font-[600]">Kontakt</p>
+              <p className="text-[32px] mt-3 mb-1 md:mt-0 font-[600]">
+                Kontakt
+              </p>
               <p className="md:mt-1">Egestam Mattsson El & Data AB</p>
               <p>Ridspögatan 15a</p>
               <p>213 77 Malmö</p>
-              <p className=""> Tel: 040-6170166</p>
+              <p className="mt-2"> Tel: 040-6170166</p>
               <p>E-post: info@emeldata.se</p>
             </div>
-            <div className="flex gap-16">
+            <div className="flex gap-16 md:gap-20">
               <div className="md:text-[16px] mt-[45px] font-[400]  flex flex-col gap-2 text-nowrap">
                 <p className="md:text-[18px] font-[600]">Meny</p>
                 <p>Vårt erbjudande</p>
@@ -98,7 +100,7 @@ export default function App() {
                 <p>Om oss</p>
                 <p>Kontakta oss</p>
               </div>
-              <div className="bg-white h-[1px] absolute -left-10 md:left-0 mx-10  bottom-[280px] md:bottom-[310px]  opacity-20 w-[90%] md:w-[45%]" />
+              <div className="bg-white h-[1px] absolute -left-10  mx-10  bottom-[295px] md:bottom-[255px]  opacity-20 w-full md:w-[50%]" />
               <div className="md:text-[16px] mt-[45px] font-[400] flex flex-col gap-2 text-nowrap">
                 <p className="md:text-[18px] font-[600]">Din integritet</p>
                 <p>Integritetspolicy</p>
@@ -116,13 +118,13 @@ export default function App() {
               </div>
             </div>
           </div>
-          <div className="absolute hidden md:block right-32 bottom-64">
+          <div className="absolute hidden md:block right-32 bottom-48">
             <img
               src={em}
               width={164}
               height={104}
               alt="em"
-              className="min-w-[164px] h-[104px]"
+              className=" min-w-[164px] h-[104px]"
             />{" "}
           </div>
         </div>
